df7e332d0da054b1b99fc69b26dff692ac89d265
rafagabidulin <rafagabidulin@gmail.com>
2022-12-09 23:24:06 +0300

    added Todo type

--- index.d.ts
+++ index.d.ts
@@ -2,3 +2,10 @@ declare module '*.module.css' {
   const classes: { [key: string]: string };
   export default classes;
 }
+
+type Todo = {
+  id: number;
+  name: string;
+  description: string;
+  checked: boolean;
+};
