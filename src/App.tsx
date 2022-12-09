69058e0330c05c19cacbf661bf1bb6197fbc5c4a
rafagabidulin <rafagabidulin@gmail.com>
2022-12-09 23:28:18 +0300

    added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
 import React from 'react';
+import { TodoProvider } from './utils';
+import { Header } from './Header/Header';
+import { Piechart } from './PieChart/Piechart';
+import { TodoList } from './TodoList/TodoList';
+import { TodoPanel } from './TodoPanel/TodoPanel';
 
-export const App = () => <div>App</div>;
+export const App = () => (
+  <TodoProvider>
+    <div>
+      <Header />
+      <Piechart />
+      <TodoPanel mode='add' />
+      <TodoList />
+    </div>
+  </TodoProvider>
+);
