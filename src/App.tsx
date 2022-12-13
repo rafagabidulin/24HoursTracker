276dbd7e4c3ad15297a9651482c3b6fc06c20635
rafagabidulin <rafagabidulin@gmail.com>
2022-12-13 21:27:50 +0300

    modified App

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
-import { TodoProvider } from './utils';
 import { Header } from './Header/Header';
-import { Piechart } from './PieChart/Piechart';
-import { TodoList } from './TodoList/TodoList';
-import { TodoPanel } from './TodoPanel/TodoPanel';
+import { DoughnutChart } from './DoughnutChart/DoughnutChart';
+
+export const data = {
+  labels: ['Sleep', 'Work', 'Dinner', 'Study', 'Reading', 'Free'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [8, 9, 1, 4, 1, 1],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }
+  ]
+};
 
 export const App = () => (
-  <TodoProvider>
-    <div>
-      <Header />
-      <Piechart />
-      <TodoPanel mode='add' />
-      <TodoList />
-    </div>
-  </TodoProvider>
+  <div>
+    <Header />
+    <DoughnutChart data={data} />
+  </div>
 );
