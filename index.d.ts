fa817890ed0ef1bb015dd953c2d3000055a3489c
rafagabidulin <rafagabidulin@gmail.com>
2022-12-13 21:27:13 +0300

    deleted Todo type

--- index.d.ts
+++ index.d.ts
@@ -2,10 +2,3 @@ declare module '*.module.css' {
   const classes: { [key: string]: string };
   export default classes;
 }
-
-type Todo = {
-  id: number;
-  name: string;
-  description: string;
-  checked: boolean;
-};
